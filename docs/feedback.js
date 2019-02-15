cf52025390cec00f8c963851f2fb8384b4290730
gauteoh <37087101+gauteoh@users.noreply.github.com>
2019-02-15 12:08:24 +0100

    Update feedback.js

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/feedback';
+const apiUrl = 'https://920ec695.eu-gb.apiconnect.appdomain.cloud/20-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
